2a867ab872b4a4da440f9c6c95a7bcd0b028dd4a
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2025-05-19 13:36:55 +0100

    changed dns records todropserver.shop

--- index.py
+++ index.py
@@ -2780,7 +2780,7 @@ if __name__ == '__main__':
         socketio.run(
         app,
         host='127.0.0.1',
-        port=8443,
+        port=5000,
         debug=True
     )
     except Exception as e:
